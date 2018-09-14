04ae114eeff1aa536ba22de97d74ff1a12212fa0
SenorCode <appmar23@gmail.com>
2018-09-13 21:35:02 -0400

    added content to footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import{Container} from 'semantic-ui-react'
+import{Container, Grid, GridRow, Header} from 'semantic-ui-react'
 import styled from 'styled-components'
 
 
@@ -13,6 +13,45 @@ const Footerstyled = styled.div`
 const footer =() => {
     return (
         <Footerstyled>
+            <Container>
+                <Grid  relaxed='very' columns={4}>
+                    <Grid.Row >
+                        <Grid.Column>
+                            <Header inverted>Customer Service</Header>
+                            <p>Help & FAQ</p>
+                            <p>Contact Us</p>
+                            <p>Para Ayuda</p>
+                            <p>Shipping and Delivery</p>
+                        </Grid.Column>
+
+                        <Grid.Column >
+                        <Header inverted>Customer Service</Header>
+                            <p>Help & FAQ</p>
+                            <p>Contact Us</p>
+                            <p>Para Ayuda</p>
+                            <p>Shipping and Delivery</p>
+                                
+                        </Grid.Column>
+
+                        <Grid.Column >
+                        <Header inverted>Customer Service</Header>
+                            <p>Help & FAQ</p>
+                            <p>Contact Us</p>
+                            <p>Para Ayuda</p>
+                            <p>Shipping and Delivery</p>
+                                
+                        </Grid.Column>
+                        <Grid.Column>
+                        <Header inverted>React Cinemas Inc</Header>
+                            <p>Corporate Site</p>
+                            <p>Careers</p>
+                            <p>Press Room</p>
+                            <p>Investors</p>
+                                
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
 
         </Footerstyled>
 
