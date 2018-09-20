e94efc6bbce3336cd5c0654a2154c27c165b8c0b
SenorCode <appmar23@gmail.com>
2018-09-20 13:04:13 -0400

    social media edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Grid, Container} from 'semantic-ui-react'
+import {Grid, Container, Button, Icon, Header} from 'semantic-ui-react'
 
 
 const Background = styled.div`
@@ -16,12 +16,15 @@ const Headerstyled = styled.h1`
     vertical-align:middle;
     line-height:100px;
 `
-const Colheader = styled.h1`
+const Colheader = styled.h2`
     
     font-weight: lighter;
     text-align:center;
     vertical-align:middle;
-    line-height:100px;
+    line-height:50px;
+`
+const Socialmedia = styled.div`
+    text-align:center;
 `
 
 const mainbody = () => {
@@ -39,16 +42,32 @@ const mainbody = () => {
             <Grid>
                 <Grid.Column width ={4}>
                     <Colheader> Golden Pass</Colheader>
+
                  
                 </Grid.Column>
                 <Grid.Column width ={8}>
-                    <Background>
+                   
                         <Colheader> Featured Movies</Colheader>
-                    </Background>
+
+                    
                 </Grid.Column>
                 <Grid.Column width ={4}>
                    
-                        <Colheader> Stay Connected</Colheader>
+                    <Colheader> Stay Connected</Colheader>
+                        <Socialmedia>
+                            <Icon circular  color='blue' name='facebook'size='big' link/> 
+                            
+                            <Icon name='twitter' color='blue' size='big' circular link/>
+                            
+                            <Icon name='youtube' color='red' size='big' circular link/>
+                            
+                            <Icon name='instagram' color='teal'  size='big' circular link />
+                        </Socialmedia>
+                    <Colheader>Sign Up For Emails</Colheader>
+                        <Socialmedia>
+                            <Icon color='black' name='mail' size='big'/>
+                        </Socialmedia>
+                        
                     
                 </Grid.Column>
             </Grid>
